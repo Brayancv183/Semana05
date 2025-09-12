1d4b9108aa3d5b79ad859e6b7fb3ef57eaa91356
Brayancv183 <brayancv183@gmail.com>
2025-09-12 14:32:45 -0500

    Semana_4_metodos_de_ordenamiento

---
+++ Semana4_Ordenamientos/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    int numeros [];
+    for int = 0; 0 < lista
+
+}
